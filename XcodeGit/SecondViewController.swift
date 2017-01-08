c61ad1f0584631ee53afeac8e9f7d874499732bf
con9con <condrecreese@gmail.com>
2017-01-08 12:24:58 -0500

    Initial Commit

---
+++ XcodeGit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  XcodeGit
+//
+//  Created by Condrec Reese on 1/8/17.
+//  Copyright © 2017 Condrec Reese. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
